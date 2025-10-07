579c3889f64dfb3fdc8b9285d53bb1102c4042d4
Dineshkumar <dineshkumar31t@gmail.com>
2025-10-07 19:40:30 +0530

    Update Product.jsx

--- Food Cart/src/Components/Product.jsx
+++ Food Cart/src/Components/Product.jsx
@@ -1,15 +1,22 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import './Product.css'
 import { GreantContext } from '../App'
 
 export const Product = ({item}) => {
   const[Cart,setCart]=useContext(GreantContext)
+  const[copied,setCopied]=useState(false)
+  const couponCode="FOOD31"
 
 const addCart=()=>{
   setCart([...Cart,item])
 }
 const removeCart=()=>{
 setCart(Cart.filter((cartitem)=>cartitem.ID!==item.ID))
+}
+const clickMe=()=>{
+  navigator.clipboard.writeText(couponCode)
+  setCopied(true)
+  setTimeout(()=>setCopied(false),5000)
 }
 
   return (
@@ -21,6 +28,11 @@ setCart(Cart.filter((cartitem)=>cartitem.ID!==item.ID))
             <h1 className='title'>{item.NAME}</h1>
             <p className='price'>Price Rs: {item.AMT}</p>
             {Cart.some((cartitem)=>cartitem.ID === item.ID)?(<button className='remove-but' onClick={removeCart}>Remove Cart</button>):(<button className='item-but' onClick={addCart}>Add Cart</button>)}
+        </div>
+        <div >
+          <button className=' code text-info'  onClick={clickMe} > CoupenCode : {couponCode} <span className='span'>{copied ? "copied!" : ""}</span>
+            
+          </button>
         </div>
 
 
